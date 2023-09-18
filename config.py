14f1e2d8c888e6a5998695ba9d348d09f8efa6da
mohammad <9m.kh557@gmai.com>
2023-09-18 16:45:19 +0330

    mlp worked

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ action_recognition_model = "models/mlp0.93"
 pose_estimation_model = "models/yolov8m-pose.pt"
 dataset_path = "data/points2"
 window_name = "human action recognition"
-playback_frame_skip = 1
-feature_count = 100
+playback_frame_skip = 100
+feature_count = 40
